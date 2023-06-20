a9c48cf375aa9de595bf7b46d713734143149d9b
Eduardo Fonseca <eduardo.fonseca@jassy.ag>
2023-06-20 17:58:08 -0300

    Guessed word created

--- hangman.c
+++ hangman.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_WORD_LENGTH 100
 
 int main() {
     char secret_word[MAX_WORD_LENGTH];
+    char guessed_word[MAX_WORD_LENGTH];
+    int wordLength;
 
     // Opening entrance
     printf("***********************\n");
@@ -14,6 +17,15 @@ int main() {
     printf("Enter the secret word: ");
     scanf("%s", secret_word);
     printf("%s\n", secret_word);
+
+    wordLength = strlen(secret_word);
+
+    for (int i = 0; i < wordLength; i++)
+        guessed_word[i] = '-';
+
+    guessed_word[wordLength] = '\0';
+
+    printf("%s\n", guessed_word);
     
     return 0;
 }
