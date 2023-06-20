f326c86c7f3c252424a1a929e7181aa231803443
Eduardo Fonseca <eduardo.fonseca@jassy.ag>
2023-06-20 17:51:32 -0300

    Secret word input

--- hangman.c
+++ hangman.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+#define MAX_WORD_LENGTH 100
+
 int main() {
+    char secret_word[MAX_WORD_LENGTH];
+
+    // Opening entrance
     printf("***********************\n");
     printf("* Welcome to Hangman! *\n");
     printf("***********************\n");
+
+    // Word input for testing
+    printf("Enter the secret word: ");
+    scanf("%s", secret_word);
+    printf("%s\n", secret_word);
     
     return 0;
 }
