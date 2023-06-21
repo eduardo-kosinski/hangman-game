f6afd575c96e91425aa67951db048ccb26769854
eduardo-kosinski <edu_f97@outlook.com>
2023-06-20 22:11:31 -0300

    Looping with guessing

--- hangman.c
+++ hangman.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
+#define MAX_TRIES 6
 #define MAX_WORD_LENGTH 100
 
 int main() {
     char secret_word[MAX_WORD_LENGTH];
     char guessed_word[MAX_WORD_LENGTH];
+    int tries = 0;
     int wordLength;
+    char guess;
+    bool correct_guess;
 
     // Opening entrance
     printf("***********************\n");
@@ -26,6 +33,25 @@ int main() {
     guessed_word[wordLength] = '\0';
 
     printf("%s\n", guessed_word);
+
+    while (tries < MAX_TRIES) {
+        printf("Guessed word: %s\n", guessed_word);
+        printf("Enter your guess: ");
+        scanf(" %c", &guess);
+        guess = tolower(guess);
+
+        printf("%c\n", guess);
+
+        correct_guess = false;
+
+        for (int i = 0; i < wordLength; i++) {
+            if (guess == secret_word[i]) {
+                guessed_word[i] = guess;
+                correct_guess = true;
+            }   
+        }
+
+    }
     
     return 0;
 }
