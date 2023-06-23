4c3b96f13c0abdd681203e7599a889cc9e6002a2
eduardo-kosinski <edu_f97@outlook.com>
2023-06-22 21:50:53 -0300

    New functions and beautify

--- hangman.c
+++ hangman.c
@@ -7,14 +7,26 @@
 
 #define MAX_TRIES 6
 #define MAX_WORD_LENGTH 100
+#define RANDOM_WORD
 
 char g_secretWord[MAX_WORD_LENGTH];
 
-void ChooseWord() {
-    FILE* f;
+void Entrance()
+{
+    printf("***********************\n");
+    printf("* Welcome to Hangman! *\n");
+    printf("***********************\n");
+}
 
+#ifdef RANDOM_WORD
+void ChooseWord()
+{
+    FILE *f;
+
+    // This file must be on output directory!
     f = fopen("wordbank.txt", "r");
-    if(f == 0) {
+    if (f == 0)
+    {
         printf("Database not available =(\n\n");
         exit(1);
     }
@@ -25,14 +37,30 @@ void ChooseWord() {
     srand(time(0));
     int random = rand() % wordCounter;
 
-    for(int i = 0; i <= random; i++) {
+    for (int i = 0; i <= random; i++)
+    {
         fscanf(f, "%s", g_secretWord);
     }
 
     fclose(f);
 }
 
-int main() {
+#else
+void ChooseWord()
+{
+    printf("Enter the secret word: ");
+    scanf("%s", g_secretWord);
+    printf("%s\n", g_secretWord);
+}
+#endif // RANDOM_WORD
+
+void ClearScreen()
+{
+    printf("\e[1;1H\e[2J");
+}
+
+int main()
+{
     char guessedWord[MAX_WORD_LENGTH];
     int tries = 0;
     int wordLength;
@@ -40,15 +68,9 @@ int main() {
     bool correctGuess;
 
     // Opening
-    printf("***********************\n");
-    printf("* Welcome to Hangman! *\n");
-    printf("***********************\n");
+    Entrance();
 
-    // Word input for testing
-    // printf("Enter the secret word: ");
-    // scanf("%s", g_secretWord);
-    // printf("%s\n", g_secretWord);
-
+    // Pick a random word from database
     ChooseWord();
 
     wordLength = strlen(g_secretWord);
@@ -60,7 +82,8 @@ int main() {
 
     printf("%s\n", guessedWord);
 
-    while (tries < MAX_TRIES) {
+    while (tries < MAX_TRIES)
+    {
         printf("Guessed word: %s\n", guessedWord);
         printf("Enter your guess: ");
         scanf(" %c", &guess);
@@ -70,29 +93,30 @@ int main() {
 
         correctGuess = false;
 
-        for (int i = 0; i < wordLength; i++) {
-            if (guess == g_secretWord[i]) {
+        for (int i = 0; i < wordLength; i++)
+        {
+            if (guess == g_secretWord[i])
+            {
                 guessedWord[i] = guess;
                 correctGuess = true;
-            }   
+            }
         }
 
-        // Clean the screen
-        printf("\e[1;1H\e[2J"); 
+        ClearScreen();
 
-        if (!correctGuess) {
+        if (!correctGuess)
+        {
             tries++;
             printf("Incorrect guess! You have %d tries left.\n", MAX_TRIES - tries);
         }
-            
-        if (strcmp(g_secretWord, guessedWord) == 0) {
+
+        if (strcmp(g_secretWord, guessedWord) == 0)
+        {
             printf("Congratulations! You won!\n");
             break;
         }
-        
-
     }
-    
+
     if (tries == MAX_TRIES)
         printf("You lost! The word was %s\n", g_secretWord);
 
