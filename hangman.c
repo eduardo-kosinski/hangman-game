fe6cb990134bee575f034cd7f91467a43771df47
eduardo-kosinski <edu_f97@outlook.com>
2023-07-24 21:46:30 -0300

    Option to continue playing

--- hangman.c
+++ hangman.c
@@ -2,24 +2,64 @@
 
 int main()
 {
-    char guess;
-    char guessedLetters[MAX_WORD_LENGTH] = {0};
-    int triesLeft = MAX_TRIES;
-    int numGuessedLetters = 0;
+    char playAgain;
 
     printEntrance();
 
+    do
+    {
+        char guess;
+        char guessedLetters[MAX_WORD_LENGTH] = {0};
+        int triesLeft = MAX_TRIES;
+        int numGuessedLetters = 0;
+
 #ifdef RANDOM_WORD
-    char *secretWord = getRandomWord();
+        char *secretWord = getRandomWord();
 #else
-    char *secretWord = chooseWord();
+        char *secretWord = chooseWord();
 #endif // RANDOM_WORD
 
-    char *guessedWord = initializeGuessedWord(strlen(secretWord));
+        char *guessedWord = initializeGuessedWord(strlen(secretWord));
 
-    while (triesLeft)
-    {
-        do
+        while (triesLeft)
+        {
+            do
+            {
+                drawHangman(triesLeft);
+
+                printf("You have %d tries left.\n", triesLeft);
+                printf("Guessed word: %s\n", guessedWord);
+                printf("Tried letters: ");
+
+                for (int i = 0; i < numGuessedLetters; i++)
+                    printf("%c ", guessedLetters[i]);
+
+                printf("\nEnter your guess: ");
+                scanf(" %c", &guess);
+                guess = tolower(guess);
+
+                clearScreen();
+            } while (!isValidGuess(guess, guessedLetters, numGuessedLetters));
+
+            guessedLetters[numGuessedLetters] = guess;
+            numGuessedLetters++;
+
+            if (isCorrectGuess(guess, secretWord, guessedWord))
+                printf("Correct guess!\n");
+            else
+            {
+                triesLeft--;
+                printf("Incorrect guess!\n");
+            }
+
+            if (strcmp(secretWord, guessedWord) == NULL)
+            {
+                printf("Congratulations! You won!\nThe word was %s\n", secretWord);
+                break;
+            }
+        }
+
+        if (triesLeft == 0)
         {
             drawHangman(triesLeft);
 
@@ -30,47 +70,20 @@ int main()
             for (int i = 0; i < numGuessedLetters; i++)
                 printf("%c ", guessedLetters[i]);
 
-            printf("\nEnter your guess: ");
-            scanf(" %c", &guess);
-            guess = tolower(guess);
-
-            clearScreen();
-        } while (!isValidGuess(guess, guessedLetters, numGuessedLetters));
-
-        guessedLetters[numGuessedLetters] = guess;
-        numGuessedLetters++;
-
-        if (isCorrectGuess(guess, secretWord, guessedWord))
-            printf("Correct guess!\n");
-        else
-        {
-            triesLeft--;
-            printf("Incorrect guess!\n");
+            printf("\nYou lost! The word was %s\n", secretWord);
         }
 
-        if (strcmp(secretWord, guessedWord) == NULL)
-        {
-            printf("Congratulations! You won!\nThe word was %s\n", secretWord);
-            break;
-        }
-    }
+        printf("\nDo you want to play again? (y/n): ");
+        scanf(" %c", &playAgain);
 
-    if (triesLeft == 0)
-    {
-        drawHangman(triesLeft);
+        clearScreen();
 
-        printf("You have %d tries left.\n", triesLeft);
-        printf("Guessed word: %s\n", guessedWord);
-        printf("Tried letters: ");
+        free(secretWord);
+        free(guessedWord);
 
-        for (int i = 0; i < numGuessedLetters; i++)
-            printf("%c ", guessedLetters[i]);
+    } while (playAgain == 'y' || playAgain == 'Y');
 
-        printf("\nYou lost! The word was %s\n", secretWord);
-    }
-
-    free(secretWord);
-    free(guessedWord);
+    printf("\nThank you for playing! =)");
 
     return 0;
 }
